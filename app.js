afd11f3dfe2214c6f022497787025be7cdf66d69
isbergssallad <116280808+isbergssallad@users.noreply.github.com>
2023-02-16 15:59:01 +0100

    17
    Datum: 2023-02-16
    Tid: 15:58

--- app.js
+++ app.js
@@ -19,3 +19,13 @@ function cartNumbers() {
     productNumbers = parseInt(productNumbers);
     localStorage.setItem("cartNumbers", 1);
 }
+
+
+
+function changeImage(element) {
+
+    var main_prodcut_image = document.getElementById('main_product_image');
+    main_prodcut_image.src = element.src;
+    
+
+}
---
+++ cart.js
@@ -0,0 +1,18 @@
+let cart=document.querySelectorAll(".addToCart");
+console.log(cart);
+
+
+//loop
+
+let products=[
+    {
+        name:""
+    }
+]
+
+for(let i=0;i<cart.length;i++)
+{
+    cart[i].addEventListener("click", ()=>{
+    
+    })
+}
