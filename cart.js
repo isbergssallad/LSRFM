db9fa3bd88e73990f7ac4d7ab7eea955cfee1c16
isbergssallad <116280808+isbergssallad@users.noreply.github.com>
2023-02-22 19:00:29 +0100

    24
    Datum: 2023-02-22
    Tid: 19:00

--- cart.js
+++ cart.js
@@ -1,18 +1,33 @@
-let cart=document.querySelectorAll(".addToCart");
-console.log(cart);
+const cartString = localStorage.getItem("cart")
 
-
-//loop
-
-let products=[
-    {
-        name:""
-    }
-]
-
-for(let i=0;i<cart.length;i++)
-{
-    cart[i].addEventListener("click", ()=>{
-    
-    })
+let cart = [] //cart = array av cart id
+if (cartString) {
+    cart = JSON.parse(cartString)
 }
+
+cart.forEach((productid, index) => {
+    const product = window.products.find(p => p.id === productid)
+
+    const cartItem = document.createElement("li")
+    cartItem.textContent = product.name
+    cartItem.className = "cart-item-name"
+
+    const removeButton = document.createElement ("button")
+    removeButton.onclick = () => {
+        const newCart = [].concat(
+            cart.slice(0, index),
+            cart.slice(index + 1) 
+            )
+        localStorage.setItem("cart", JSON.stringify(newCart))
+        location.reload()
+
+    }
+    removeButton.textContent = "Remove"
+    removeButton.className = "cart-remove-button"
+
+    cartItem.appendChild(removeButton)
+
+    document.getElementById("cart").appendChild(cartItem)
+})
+
+//cart just nu är en array av id
--- product-page.js
+++ product-page.js
@@ -3,7 +3,23 @@ const productid = Number(params.get("product_id"))
 
 const product = window.products.find(p => p.id === productid) //global
 
-document.getElementById("product-title").textContent = product.name;
+document.getElementById("product-name").textContent = product.name;
 document.getElementById("product-description").textContent = product.description;
 document.getElementById("product-price").textContent = product.price;
 document.getElementById("product-image").src = product.image;
+
+const cartButton = document.getElementById("buy-button")
+
+cartButton.onclick = () => {
+    const previousCartString = localStorage.getItem("cart")
+
+    let previousCart = []
+    if (previousCartString) {
+        previousCart = JSON.parse(previousCartString)
+    }
+
+    localStorage.setItem("cart", JSON.stringify(previousCart.concat(productid)))
+
+    cartButton.textContent = "Added"
+
+}
