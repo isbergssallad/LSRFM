b0518e1218495cf218d9a31604c4fe9f165be2b5
isbergssallad <116280808+isbergssallad@users.noreply.github.com>
2023-02-22 18:02:34 +0100

    23
    Datum: 2023-02-22
    Tid: 18:02

---
+++ product-list.js
@@ -0,0 +1,29 @@
+window.products = [
+    {
+        id: 0,
+        name: "Polaroid One Step 2",
+        price: "$120.00",
+        description: "The One-step 2 is an analog instant camera for the modern era, blending classic design with contemporary style. It's inspired by Polaroid's original One-step, but updated to create a simple, easy-to-use instant camera that works straight out of the box. It's got a high quality lens and a powerful flash to give you great photos every time, and its long-lasting rechargeable battery means you'll always be ready to shoot, whether you're on a brief excursion or a journey round the world.",
+        image: "./product images/polaroid one step 2.png",
+    },
+    {
+        id: 1,
+        name: "Canon QL17 G-III",
+        price: "$295.00",
+        description: "35mm rangefinder camera released by Canon in 1972 featuring shutter-priority auto exposure and full manual, although the meter is inactive in manual mode. Also features Canon's quick-loading (QL) system. Metal body available in silver & black.",
+        image: "./product images/canon ql17 giii.png",
+    },
+    {
+        id: 2,
+        name: "Polaroid Super Color 635",
+        price: "$119.00",
+        description: "The Polaroid Supercolor 635 is an instant camera. The front, which you can see when the camera is opened, is gray-silver, the rest of the camera is black. In addition, the Supercolor 635 is adorned with a rainbow stripe. The Polaroid camera is easy to use. As a beginner, you practically don’t need the instructions. Just aim at the subject, press the shutter button and the picture comes out of the camera. The Supercolor 635 comes with a built-in flash. The camera does not have a tripod socket. The Polaroid Supercolor 635 requires Polaroid 600 film. Currently, the Polaroid Color Film for 600 and the Polaroid B&W Film for 600 are the best choice. The battery for the instant camera and built-in flash is located in the film cassette, so it is renewed every time the film is changed.",
+        image: "./product images/polaroid super color 635.png",
+    },
+    {
+        id: 3,
+        name: "Polaroid Super Color 1000",
+        price: "$250.00",
+        image: "./product images/polaroid super color 1000.png",
+    },
+];
--- product-page.js
+++ product-page.js
@@ -1,25 +1,7 @@
-const products = [
-    {
-        id: 0,
-        name: "Polaroid One Step 2",
-        price: "$120.00",
-        description: "The One-step 2 is an analog instant camera for the modern era, blending classic design with contemporary style. It's inspired by Polaroid's original One-step, but updated to create a simple, easy-to-use instant camera that works straight out of the box. It's got a high quality lens and a powerful flash to give you great photos every time, and its long-lasting rechargeable battery means you'll always be ready to shoot, whether you're on a brief excursion or a journey round the world.",
-        image: "./product images/polaroid one step 2.png",
-    },
-    {
-        id: 1,
-        name: "Canon QL17 G-III",
-        price: "$295.00",
-        description: "35mm rangefinder camera released by Canon in 1972 featuring shutter-priority auto exposure and full manual, although the meter is inactive in manual mode. Also features Canon's quick-loading (QL) system. Metal body available in silver & black.",
-        image: "./product images/canon ql17 giii.png",
-    }
-];
-
-
 const params = new URLSearchParams(window.location.search)
 const productid = Number(params.get("product_id"))
 
-const product = products.find(p => p.id === productid)
+const product = window.products.find(p => p.id === productid) //global
 
 document.getElementById("product-title").textContent = product.name;
 document.getElementById("product-description").textContent = product.description;
